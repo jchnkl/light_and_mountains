fb4e853a45236a75bf15cc27efe2ea119cfcbd05
Jochen Keil <jochen.keil@gmail.com>
2018-01-28 22:57:05 +0100

    Try to improve implementation; endless recursion however

--- js/lam.js
+++ js/lam.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-  function loadImages(loadMoreFn, start, n) {
-  // function loadImages(update_fn, start, n) {
+  function loadImages(start, n) {
     var promises = [];
 
     for (var i = start; i < start + n; ++i) {
@@ -9,78 +8,96 @@ $(document).ready(function() {
       promises.push($.ajax(url))
     }
 
+    return [start + n, promises];
+  }
+
+  function handleImages(promises, idx, cbFn) {
     $.when.apply($, promises).then(function() {
       results = arguments;
       for (idx in arguments) {
         $('#lam-gallery').append(arguments[idx][0]);
       }
 
-      // update_fn();
-      $('#lam-gallery').justifiedGallery('norewind');
-			$(window).scroll(loadMoreFn);
-
+      cbFn(idx);
     });
-
-		console.log("return " + (start + n));
-    return start + n;
   }
 
-	// initialize justifiedGallery
+  function updateFn(startIdx) {
+    $('#lam-gallery').justifiedGallery('norewind');
+    $(window).scroll(function() {
+      if ($(window).scrollTop() + $(window).height() >= 0.9 * $(document).height()) {
+        $(window).unbind('scroll');
+        [idx, promises] = loadImages(startIdx, 10);
+        handleImages(promises, idx, updateFn);
+      }
+    });
+  }
+
+  // initialize justifiedGallery
   function initGallery() {
     $("#lam-gallery").justifiedGallery({
-        selector: 'figure, div:not(.spinner)',
-        margins: 3,
-        rowHeight: 240,
-        cssAnimation: false,
-        imagesAnimationDuration: 0,
-        waitThumbnailsLoad: false,
-        sizeRangeSuffixes: {
-            100  : '_t',
-            240  : '_m',
-            320  : '_n',
-            640  : '_z',
-            1024 : '_b'
-        }
+      selector: 'figure, div:not(.spinner)',
+      margins: 3,
+      rowHeight: 240,
+      cssAnimation: false,
+      imagesAnimationDuration: 0,
+      waitThumbnailsLoad: false,
+      sizeRangeSuffixes: {
+        100  : '_t',
+        240  : '_m',
+        320  : '_n',
+        640  : '_z',
+        1024 : '_b'
+      }
     });
-		/*
-    return function() {
-      $('#lam-gallery').justifiedGallery('norewind');
-    }
-		*/
   }
 
-	// var update_fn = initGallery();
+  initGallery();
+
+  [idx, promises] = loadImages(0, 20);
+
+  handleImages(promises, idx, updateFn);
+
+  // handleImages(promises, function() {
+  //   $('#lam-gallery').justifiedGallery('norewind');
+  // });
+
+  // handleImages(updateFn, promises);
+
+
+
+  // var update_fn = initGallery();
 
   // start to build gallery
 
-	// function runGallery() {
+  // function runGallery() {
 
-	initGallery();
+//   initGallery();
+//
+//   var lam_index = loadImages(loadMore(0.9), 0, 20);
+//
+//   function loadMore(at) {
+//     if ($(window).scrollTop() + $(window).height() >= at * $(document).height()) {
+//       console.log("$(window).scrollTop(): " + $(window).scrollTop()); 
+//       console.log("$(window).height(): " + $(window).height()); 
+//       console.log("$(document).height(): " + $(document).height()); 
+//       $(window).unbind('scroll');
+//       lam_index = loadImages(loadMore(at), lam_index, 10);
+//     }
+//   }
 
-	var lam_index = loadImages(loadMore(0.9), 0, 20);
-
-	function loadMore(at) {
-		if ($(window).scrollTop() + $(window).height() >= at * $(document).height()) {
-			console.log("$(window).scrollTop(): " + $(window).scrollTop()); 
-			console.log("$(window).height(): " + $(window).height()); 
-			console.log("$(document).height(): " + $(document).height()); 
-			$(window).unbind('scroll');
-			lam_index = loadImages(loadMore(at), lam_index, 10);
-		}
-	}
-
-	/*
-		$(window).scroll(function() {
-			if ($(window).scrollTop() + $(window).height() == $(document).height() + 100) {
-			// if ($(window).scrollTop() + $(window).height() >= 0.9 * $(document).height()) {
-				console.log("$(window).scrollTop(): " + $(window).scrollTop()); 
-				console.log("$(window).height(): " + $(window).height()); 
-				console.log("$(document).height(): " + $(document).height()); 
-				$(window).unbind('scroll');
-				lam_index = loadImages(update_fn, lam_index, 10);
-			}
-		});
-	*/
-	// }
+  /*
+                $(window).scroll(function() {
+                        if ($(window).scrollTop() + $(window).height() == $(document).height() + 100) {
+  // if ($(window).scrollTop() + $(window).height() >= 0.9 * $(document).height()) {
+                                console.log("$(window).scrollTop(): " + $(window).scrollTop()); 
+                                console.log("$(window).height(): " + $(window).height()); 
+                                console.log("$(document).height(): " + $(document).height()); 
+                                $(window).unbind('scroll');
+                                lam_index = loadImages(update_fn, lam_index, 10);
+                        }
+                });
+                */
+  // }
 
 });

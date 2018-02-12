205a31ed30ff21ece66a69ec25c9b645a1918edf
Jochen Keil <jochen.keil@gmail.com>
2018-02-12 09:14:37 +0100

    Caps all argv args for readability

--- scripts/gen_img_meta.py
+++ scripts/gen_img_meta.py
@@ -11,28 +11,28 @@ gi.require_version('GExiv2', '0.10')
 from gi.repository import GExiv2
 
 # web dir
-w_dir = argv[1]
+W_DIR = argv[1]
 
 # img dir
-i_dir = argv[2]
+I_DIR = argv[2]
 
 # web thumb dir
-t_dir = argv[3]
+T_DIR = argv[3]
 
 # img suffix, .e.g. png
-suffix = argv[4]
+SUFFIX = argv[4]
 
-images = argv[5:]
+IMAGES = argv[5:]
 
 img_data = []
 
 idx = 0
 
-for image in images:
+for image in IMAGES:
     i = basename(image)
-    t = i.replace('.' + suffix, '_t.' + suffix)
+    t = i.replace('.' + SUFFIX, '_t.' + SUFFIX)
 
-    metadata = GExiv2.Metadata(i_dir + sep + i)
+    metadata = GExiv2.Metadata(I_DIR + sep + i)
 
     try:
         title = metadata['Xmp.dc.title'].replace('lang="x-default" ', '')
@@ -49,14 +49,14 @@ for image in images:
     except:
         rating = None
 
-    img = Image.open(i_dir + sep + i)
+    img = Image.open(I_DIR + sep + i)
 
     i_width = img.size[0]
     i_height = img.size[1]
 
     img.close()
 
-    img = Image.open(t_dir + sep + t)
+    img = Image.open(T_DIR + sep + t)
 
     t_width = img.size[0]
     t_height = img.size[1]
@@ -70,8 +70,8 @@ for image in images:
             { 'idx': idx
             , 'i': i
             , 't': t
-            , 'w_dir': w_dir
-            , 't_dir': t_dir
+            , 'w_dir': W_DIR
+            , 't_dir': T_DIR
             , 'i_width': i_width
             , 'i_height': i_height
             , 'i_aspect': i_width / i_height
